9d59be36397c231cc2bee7616d21038da5dc3f9b
Gabriel9706374 <gabriel9706374@rede.ulbra.br>
2026-08-09 19:59:57 -0300

    Fase 1

---
+++ Projeto Integrador/Funcionario.java
@@ -0,0 +1,61 @@
+package projeto.integrador;
+
+public class Funcionario {
+    private static int contador = 1;
+    private int id;
+    private String nome;
+    private String cpf;
+    private double salario;
+    private String cargo;
+
+    public Funcionario(){
+        id = contador;
+        contador++;
+    }
+
+    public int getId(){
+        return id;
+    }
+
+    public String getNome(){
+        return nome;
+    }
+
+    public void setNome(String nome){
+        this.nome = nome;
+    }
+
+    public String getCpf(){
+        return cpf;
+    }
+
+    public void setCpf(String cpf){
+        this.cpf = cpf;
+    }
+
+    public double getSalario(){
+        return salario;
+    }
+
+    public void setSalario(double salario){
+        this.salario = salario;
+    }
+
+    public String getCargo(){
+        return cargo;
+    }
+
+    public void setCargo(String cargo){
+        this.cargo = cargo;
+    }
+
+    public void mostrarDados(){
+        System.out.println("---------------------------");
+        System.out.println("ID: " + id);
+        System.out.println("Nome: " + nome);
+        System.out.println("CPF: " + cpf);
+        System.out.println("Salario: R$ " + salario);
+        System.out.println("Cargo: " + cargo);
+        System.out.println("---------------------------");
+    }
+}
